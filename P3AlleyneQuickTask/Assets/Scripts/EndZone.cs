42d4efe90a73a3eca3f4285ce31a75156622f37b
Diego Alleyne <714289@student.dusd.net>
2024-08-26 11:56:45 -0700

    GameOver
    Started working on game over for when player collides with endzone

---
+++ P3AlleyneQuickTask/Assets/Scripts/EndZone.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EndZone : MonoBehaviour
+{
+    private GameManager gameManager;
+    // Start is called before the first frame update
+    void Start()
+    {
+        gameManager = GameObject.Find("GameManager").GetComponent<GameManager>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void OnCollisionEnter(Collision collision)
+    {
+        gameManager.GameOver();
+        Debug.Log("Collision");
+    }
+}
